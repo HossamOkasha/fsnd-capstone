8ac0f2b428ab6505e41c924bd8b98209351d92d6
HossamOkasha <enghossamokashaa@gmail.com>
2020-07-21 22:23:51 +0200

    add after_request decorator

--- app.py
+++ app.py
@@ -24,7 +24,12 @@ def create_app():
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
-  
+
+    @app.after_request
+    def after_request(response):
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+      response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+      return response  
     
     @app.route('/')
     def index():
