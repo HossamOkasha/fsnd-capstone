3c7efced6da954f078ec477852476f5f06c1362d
HossamOkasha <enghossamokashaa@gmail.com>
2020-07-21 22:38:40 +0200

    fix some errors

--- test_app.py
+++ test_app.py
@@ -1,9 +1,8 @@
-import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 
-from flaskr import create_app
+from app import create_app
 from models import setup_db, Actor, Movie
 
 
